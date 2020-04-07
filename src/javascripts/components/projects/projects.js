b60fe15fdfc149a6537e6eea1eac61c9ce1a32a0
Kayla Melton <kannmelton2@gmail.com>
2020-04-06 19:19:22 -0500

    deployed and removed commented code

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -2,24 +2,6 @@ import projectData from '../../helpers/data/projectData';
 
 import utils from '../../helpers/utils';
 
-// const createProjectCards = () => {
-//   const projects = projectData.getProjects();
-//   let domStr = '';
-//   projects.forEach((project) => {
-//     domStr += `
-//     <div class="projectCard">
-//     <header><h3>${project.title.toUpperCase()}</h3></header>
-//     <img src=${project.screenshot}>
-//     <p>${project.description}</p>
-//     <p>${project.technologiesUsed}</p>
-//     <footer class="project-links">
-//     <a href="${project.url}">Link to Live Website</a>
-//     <a href="${project.githubUrl}">Link to Github Repository</a>
-//     </footer>
-//     </div>'`;
-//   });
-//   utils.printToDom('projectsPage', domStr);
-// };
 
 const createProjectCards = () => {
   projectData.getProjects()
