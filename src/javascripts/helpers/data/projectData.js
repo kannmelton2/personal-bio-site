aa694fac4ee156824ce6ee3ea9195cba14d7fd12
Kayla Melton <kannmelton2@gmail.com>
2021-02-12 11:40:16 -0600

    updated project and image data files

--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -9,10 +9,12 @@ const getDevProjects = () => new Promise((resolve, reject) => {
     .then((response) => {
       const allProjects = response.data;
       const projects = [];
-      Object.keys(allProjects).forEach((projectId) => {
-        allProjects[projectId].id = projectId;
-        projects.push(allProjects[projectId]);
-      });
+      if (allProjects) {
+        Object.keys(allProjects).forEach((projectId) => {
+          allProjects[projectId].id = projectId;
+          projects.push(allProjects[projectId]);
+        });
+      }
       resolve(projects);
     })
     .catch((err) => reject(err));
@@ -23,10 +25,12 @@ const getDesignProjects = () => new Promise((resolve, reject) => {
     .then((response) => {
       const allProjects = response.data;
       const projects = [];
-      Object.keys(allProjects).forEach((projectId) => {
-        allProjects[projectId].id = projectId;
-        projects.push(allProjects[projectId]);
-      });
+      if (allProjects) {
+        Object.keys(allProjects).forEach((projectId) => {
+          allProjects[projectId].id = projectId;
+          projects.push(allProjects[projectId]);
+        });
+      }
       resolve(projects);
     })
     .catch((err) => reject(err));
